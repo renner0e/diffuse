24bb302155f782969955f10da7e7e71a7a5e7dcc
renner <renner0@posteo.de>
2025-06-21 12:07:31 +0200

    fix: see if we need index.theme override in aurora

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 
-rm -f /usr/share/icons/hicolor/index.theme
+dnf5 reinstall libXcursor -y
 
 dnf5 remove -y \
   default-editor \

6f2bd8e1a20fb7927220120aaf4ff092d44a70ce
renner <renner0@posteo.de>
2025-10-19 23:02:20 +0200

    chore: remove all the flatpak preinstall crap that is in aurora beta

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,9 +4,6 @@ set -ouex pipefail
 
 rsync -rvK /ctx/system_files/ /
 
-
-dnf5 reinstall libXcursor -y
-
 dnf5 remove -y \
   default-editor \
   nano \
@@ -15,29 +12,6 @@ dnf5 remove -y \
   vim-enhanced \
   vim-data \
   ptyxis
-
-# Bazaar flatpak migration test
-# I also want flatpak master to play around with preinstall
-
-dnf5 -y remove bazaar
-
-cp -r /usr/share/ublue-os/bazaar /etc
-
-sed -i 's|/usr/share/ublue-os/|/run/host/etc/|g' /etc/bazaar/config.yaml
-
-systemctl --global enable bazaar.service
-
-dnf5 -y copr enable ublue-os/flatpak-test
-
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak flatpak
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-libs flatpak-libs
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-session-helper flatpak-session-helper
-
-dnf5 -y copr disable ublue-os/flatpak-test
-
-rpm -qa --qf "%{NAME} %{VENDOR}\n" | grep -i copr
-
-systemctl enable flatpak-preinstall.service
 
 dnf5 install -y \
   mpdris2 \

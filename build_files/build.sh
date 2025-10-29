96a1f2215c75dda7133f6a403f6dde066b74ccd2
renner <renner0@posteo.de>
2025-10-29 18:56:24 +0100

    feat: set vim as default editor
    - don't remove nano
    
    - add ptyxis back

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,13 +5,10 @@ set -ouex pipefail
 rsync -rvK /ctx/system_files/ /
 
 dnf5 remove -y \
-  default-editor \
-  nano \
-  nano-default-editor \
   vim-minimal \
   vim-enhanced \
-  vim-data \
-  ptyxis
+  nano-default-editor \
+  vim-data
 
 dnf5 install -y \
   mpdris2 \
---
+++ system_files/etc/profile.d/vim-default-editor.sh
@@ -0,0 +1,6 @@
+# Ensure vim is set as EDITOR if it isn't already set
+
+if ( ! ($?EDITOR) ) then
+	setenv EDITOR "/usr/bin/vim"
+endif
+

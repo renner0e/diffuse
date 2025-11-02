d7d20f86a658f94da8e16742445457c59095a752
renner <renner0@posteo.de>
2025-11-02 23:12:00 +0100

    chore: remove all the bullshit
    - neovim
    - nano removal

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,16 +4,4 @@ set -ouex pipefail
 
 rsync -rvK /ctx/system_files/ /
 
-dnf5 remove -y \
-  vim-minimal \
-  vim-enhanced \
-  nano-default-editor \
-  vim-data
-
-dnf5 install -y \
-  mpdris2 \
-  neovim \
-
-# Replace nvim with vim
-ln -s /usr/bin/nvim /usr/bin/vim
-ln -s /usr/bin/nvim /usr/bin/vi
+dnf5 install -y mpdris2
